4f46d7b893c86b43886559bcdb9e33a6af5cb195
zhangzq71 <zhangzq71@hotmail.com>
2013-12-13 15:57:25 +0800

    增加MenuCatalogPage

---
+++ gcafeApp/ViewModel/VMMenuCatalog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace gcafeApp.ViewModel
+{
+    public class VMMenuCatalog : VMBase
+    {
+        private RelayCommand<string> navigateToCommand;
+
+        public VMMenuCatalog()
+        {
+            navigateToCommand = new RelayCommand<string>(NavigateTo);
+        }
+
+        private void NavigateTo(string cata)
+        {
+
+        }
+    }
+}
