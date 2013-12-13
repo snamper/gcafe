8efcb363686ce5926d5e124c8e587988d84c9e19
zhangzq71 <zhangzq71@hotmail.com>
2013-12-13 17:23:25 +0800

    暂时正常

--- gcafeApp/ViewModel/MenuSelectViewModel.cs
+++ gcafeApp/ViewModel/MenuSelectViewModel.cs
@@ -1,15 +1,34 @@
 ﻿using System;
+using System.Windows.Input;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace gcafeApp.ViewModel
 {
     public class MenuSelectViewModel : VMBase
     {
+        private RelayCommand<string> _navigateToCommand;
+
         public MenuSelectViewModel()
         {
+            _navigateToCommand = new RelayCommand<string>(NavigateTo);
         }
+
+        public ICommand NavigateToCommand
+        {
+            get { return _navigateToCommand; }
+        }
+
+        private void NavigateTo(string type)
+        {
+            if (type == "CataSel")
+            {
+                App.RootFrame.Navigate(new Uri("/Pages/MenuCatalogPage.xaml", UriKind.Relative));
+            }
+        }
+
 
         public string Pro1
         {
