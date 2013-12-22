20e6f2916abf06958ee2dc846cbee5899f4f1f2b
zhangzq71 <zhangzq71@hotmail.com>
2013-12-22 12:18:34 +0800

    add ItemCountToTextConverter

---
+++ gcafeApp/Converters/ItemCountToTextConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Windows.Data;
+
+namespace gcafeApp.Converters
+{
+    public class ItemCountToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //if (value.GetType() != typeof(ICollection))
+            //    throw new InvalidOperationException("必须是ICollection类型");
+
+            //return string.Format("这个套餐有{0}项内容", ((ICollection)value).Count);
+
+            return string.Format("这个套餐有{0}项内容", ((ObservableCollection<gcafeSvc.SetmealItem>)value).Count);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
