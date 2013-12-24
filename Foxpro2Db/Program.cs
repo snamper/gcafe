4632299b89eeff37e030ec5b31b5bfa7bdb3042c
zhangzq71 <zhangzq71@hotmail.com>
2013-12-24 23:36:57 +0800

    改回Transparent

--- Foxpro2Db/Program.cs
+++ Foxpro2Db/Program.cs
@@ -13,8 +13,8 @@ namespace Foxpro2Db
             IConvertBase converter = new Foxpro2SQL();
 
             converter.FoxproConnStr = @"Provider=VFPOLEDB.1;Data Source=D:\gwr\DATA\lygOrder.dbc";
-            //converter.DbConnStr = @"Data Source=192.168.1.200;Initial Catalog=gcafe;Persist Security Info=True;User ID=sa;Password=85bc3e30";
-            converter.DbConnStr = @"Data Source=192.168.11.5;Initial Catalog=gcafe;Persist Security Info=True;User ID=sa;Password=GcafeGcafe2012";
+            converter.DbConnStr = @"Data Source=192.168.1.200;Initial Catalog=gcafe;Persist Security Info=True;User ID=sa;Password=85bc3e30";
+            //converter.DbConnStr = @"Data Source=192.168.11.5;Initial Catalog=gcafe;Persist Security Info=True;User ID=sa;Password=GcafeGcafe2012";
             converter.Convert();
         }
     }
