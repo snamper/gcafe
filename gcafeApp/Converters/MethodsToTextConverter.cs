f366660e9f23bf8ad7907738697d18d6185237cb
zhangzq71 <zhangzq71@hotmail.com>
2013-12-26 23:41:44 +0800

    ADD file

---
+++ gcafeApp/Converters/MethodsToTextConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace gcafeApp.Converters
+{
+    public class MethodsToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Type t = value.GetType();
+
+            return "ss";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
