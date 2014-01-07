5eb60bae2f0a30ae584071060c7e62e1639b4bb3
zhangzq71 <zhangzq71@hotmail.com>
2014-01-07 17:22:23 +0800

    写入foxpro的poh表

--- gcafePrnConsole/PrintTask.cs
+++ gcafePrnConsole/PrintTask.cs
@@ -185,54 +185,16 @@ namespace gcafePrnConsole
                         OrderNum = orderDetails[0].order.order_num,
                     };
 
+                    string foxproOrderNum = GetOrderNumFromFoxproByTablenNum(orderDetails[0].order.table_no);
+                    int serial = 0;
                     foreach (var orderDetail in orderDetails)
                     {
                         huaDan.AddItem(orderDetail);
 
                         #region 写入foxpro
                         _mutexFoxpro.WaitOne();
-                        using (var conn = new OleDbConnection(Global.FoxproPath))
-                        {
-                            conn.Open();
-
-                            string sql;
-
-                            sql = string.Format("SELECT orderno FROM orders WHERE tableno = '{0}' AND paid = 0", orderDetails[0].order.table_no);
-                            using (var cmd1 = new OleDbCommand(sql, conn))
-                            {
-                                string orderNum = (string)cmd1.ExecuteScalar();
-                                if (orderNum != null && orderNum.Length > 0)
-                                {
-                                    huaDan.OrderNum = orderNum;
-
-                                    sql = string.Format("SELECT price2, productnn FROM product WHERE productno = '{0}'", orderDetail.menu.number);
-                                    OleDbCommand cmd = new OleDbCommand(sql, conn);
-                                    OleDbDataReader reader = cmd.ExecuteReader();
-                                    if (reader.Read())
-                                    {
-                                        decimal price2 = reader.GetDecimal(0);
-                                        string prodNn = reader.GetString(1);
-
-                                        sql = string.Format("INSERT INTO orditem(ordertime, orderno, productno, prodname, price, price2, quantity, note1name, note2name, note1no, price1, quantity1, add10, quantity2, discount, machineid, taiji, memberno, amt, productnn, note2no) VALUES({0}, '{1}', '{2}', '{3}', {4}, {5}, {6}, '11', '', '', 0, 0, 0, 0, 1, '{7}', 0, '', 0, '{8}', '')",
-                                            "{ fn NOW() }",
-                                            orderNum,
-                                            orderDetail.menu.number,
-                                            orderDetail.menu.name,
-                                            orderDetail.price,
-                                            price2,
-                                            orderDetail.quantity,
-                                            "A",
-                                            prodNn);
-
-                                        cmd = new OleDbCommand(sql, conn);
-                                        cmd.ExecuteNonQuery();
-                                    }
-
-                                    conn.Close();
-                                }
-                            }
-                            _mutexFoxpro.ReleaseMutex();
-                        }
+                        SaveOrder2Foxpro(orderDetail, foxproOrderNum, ++serial);
+                        _mutexFoxpro.ReleaseMutex();
                         #endregion 写入foxpro
                     }
 
@@ -583,6 +545,174 @@ namespace gcafePrnConsole
             }
 
             return rtn;
+        }
+
+        string SaveOrder2Foxpro(order_detail orderDetail, string orderNum, int serial)
+        {
+            string rtn = string.Empty;
+
+            using (var conn = new OleDbConnection(Global.FoxproPath))
+            {
+                conn.Open();
+
+                string sql;
+
+                if (orderNum != null && orderNum.Length > 0)
+                {
+                    sql = string.Format("SELECT price2, productnn FROM product WHERE productno = '{0}'", orderDetail.menu.number);
+                    OleDbCommand cmd = new OleDbCommand(sql, conn);
+                    OleDbDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        decimal price2 = reader.GetDecimal(0);
+                        string prodNn = reader.GetString(1);
+
+                        sql = string.Format("INSERT INTO orditem(ordertime, orderno, productno, prodname, price, price2, quantity, note1name, note2name, note1no, price1, quantity1, add10, quantity2, discount, machineid, taiji, memberno, amt, productnn, note2no) VALUES({0}, '{1}', '{2}', '{3}', {4}, {5}, {6}, '11', '', '', 0, 0, 0, 0, 1, '{7}', 0, '', 0, '{8}', '')",
+                            "{ fn NOW() }",
+                            orderNum,
+                            orderDetail.menu.number,
+                            orderDetail.menu.name,
+                            orderDetail.price,
+                            price2,
+                            orderDetail.quantity,
+                            "A",
+                            prodNn);
+
+                        cmd = new OleDbCommand(sql, conn);
+                        cmd.ExecuteNonQuery();
+
+                        if (orderDetail.order_detail_setmeal.Count > 0)
+                        {
+                            // 这是套餐
+                            foreach (var setmeal in orderDetail.order_detail_setmeal)
+                            {
+                                sql = string.Format("INSERT INTO poh(department, ordertime, orderno, serialno, prodname, machineid, quantity, tableno, itemno, printgroup, remark1, remark2, waiter, serial) VALUES('{0}', {1}, '{2}', '{3}', '{4}', '{5}', {6}, '{7}', '{8}', '{9}', '{10}', '{11}', '{12}', '{13:D2}')",
+                                     setmeal.menu.number.Substring(0, 2), "{ fn NOW() }", orderNum, GenSerialNo(setmeal.menu.number), setmeal.menu.name, setmeal.order_detail.device_id, setmeal.order_detail.quantity, setmeal.order_detail.order.table_no, "0", GetPrintGroup(setmeal.menu.number), setmeal.order_detail.menu.name, GetZuoFa(setmeal.order_detail_method), setmeal.order_detail.order.staff2.number, serial);
+
+                                cmd = new OleDbCommand(sql, conn);
+                                cmd.ExecuteNonQuery();
+                            }
+                        }
+                        else
+                        {
+                            // 不是套餐
+                            sql = string.Format("INSERT INTO poh(department, ordertime, orderno, serialno, prodname, machineid, quantity, tableno, itemno, printgroup, remark1, remark2, waiter, serial) VALUES('{0}', {1}, '{2}', '{3}', '{4}', '{5}', {6}, '{7}', '{8}', '{9}', '{10}', '{11}', '{12}', '{13:D2}')",
+                                orderDetail.menu.number.Substring(0, 2), "{ fn NOW() }", orderNum, GenSerialNo(orderDetail.menu.number), orderDetail.menu.name, orderDetail.device_id, orderDetail.quantity, orderDetail.order.table_no, "0", GetPrintGroup(orderDetail.menu.number), "", GetZuoFa(orderDetail.order_detail_method), orderDetail.order.staff2.number, serial);
+
+                            cmd = new OleDbCommand(sql, conn);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    conn.Close();
+                }
+            }
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// 取回打印组
+        /// </summary>
+        /// <param name="productNo"></param>
+        /// <returns></returns>
+        string GetPrintGroup(string productNo)
+        {
+            string pg = string.Empty;
+
+            try
+            {
+                using (var conn = new OleDbConnection(Global.FoxproPath))
+                {
+                    conn.Open();
+
+                    using (var cmd = new OleDbCommand(string.Format("SELECT printgroup FROM product WHERE productno = '{0}'", productNo), conn))
+                    {
+                        pg = cmd.ExecuteScalar() as string;
+                        if (pg != null)
+                            pg = pg.Trim();
+                        else
+                            pg = string.Empty;
+                    }
+
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Global.Logger.Error(ex.Message);
+            }
+
+            return pg;
+        }
+
+        /// <summary>
+        /// 生成poh.serialno
+        /// </summary>
+        /// <param name="productNo"></param>
+        /// <returns></returns>
+        private int GenSerialNo(string productNo)
+        {
+            int serialNo = -1;
+
+            try
+            {
+                using (var conn = new OleDbConnection(Global.FoxproPath))
+                {
+                    conn.Open();
+
+                    string sql;
+
+                    if (productNo.Substring(0, 2) == "11")
+                        sql = "SELECT kitchen FROM lastsn";
+                    else if (productNo.Substring(0, 2) == "22")
+                        sql = "SELECT bar FROM lastsn";
+                    else
+                        return -1;
+
+                    using (var cmd = new OleDbCommand(sql, conn))
+                    {
+                        serialNo = Int32.Parse(cmd.ExecuteScalar() as string);
+                    }
+
+                    if (productNo.Substring(0, 2) == "11")
+                        sql = string.Format("UPDATE lastsn SET kitchen = '{0}'", serialNo + 1);
+                    else if (productNo.Substring(0, 2) == "22")
+                        sql = string.Format("UPDATE lastsn SET bar = '{0}'", serialNo + 1);
+
+                    using (var cmd = new OleDbCommand(sql, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Global.Logger.Error(ex.Message);
+                return -1;
+            }
+
+            return serialNo;
+        }
+
+        string GetZuoFa(ICollection<order_detail_method> methods)
+        {
+            string zuofa = string.Empty;
+
+            if (methods != null && methods.Count > 0)
+            {
+                foreach (var method in methods)
+                {
+                    if (string.IsNullOrEmpty(zuofa))
+                        zuofa = method.method.name;
+                    else
+                        zuofa += "，" + method.method.name;
+                }
+            }
+
+            return zuofa;
         }
 
         public void Dispose()
